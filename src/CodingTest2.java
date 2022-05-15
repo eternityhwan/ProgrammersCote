5f0f7852ae603270e6f7ca09d900c9f244d29d22
mirrorkyh@gmail.com <mirrorkyh@gmail.com>
2022-05-15 11:37:15 +0900

    kimseobang clear
    CodingTest2 클래스 중괄호 닫아주니 되더라.

--- src/CodingTest2.java
+++ src/CodingTest2.java
@@ -1,22 +1,24 @@
 public class CodingTest2 {
-
-    public static void main(String[] args) {
-
-    class Solution {
-        public String solution(String[] seoul) {
-            String answer = "";
-
-            for (int i =0; i < seoul.length; i++) {
-              if (seoul[i].equals("kim") {
-                    System.out.printf("김서방은 %s",seoul[i] "에 있다.");
-
-                    break;
-                }
-            }
+    public String solution(String[] seoul) {
+        String answer = "";
 
 
-            return answer;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은" + i + "에 있다";
+                System.out.println(seoul);
             }
         }
+        return answer;
     }
 }
+//    public static void main(String[] args) {
+//        // 스트링 배열을 선언, 헬로우랑 월드가 들어있는 스트링 배열 선언하기
+//        String[] helloworld = {"hello", "world"};
+//        // 선언한 배열을 solution이라는 메소드에 넘긴다.
+//        new CodingTest2().solution(helloworld);
+//        System.out.println(helloworld[0]);
+//        System.out.println(helloworld[1]);
+
+//    }
+//}
